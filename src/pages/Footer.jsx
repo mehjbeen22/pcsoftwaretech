a835d67c61796966730c79605cb95c56881f1c12
mehjbeen22 <mehjbeen222navgurukul.org>
2024-06-08 17:41:36 +0530

    +1

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaCreditCard, FaPaypal, FaShieldAlt } from 'react-icons/fa'; // Example icons for secure payments
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
@@ -17,11 +18,25 @@ const Footer = () => {
         <div>
           <h2 className="text-2xl font-bold mb-4 ml-6">Resources</h2>
           <ul className="space-y-2 ">
-            <li className="hover:text-gray-500">Antivirus Shop</li>
-            <li className="hover:text-gray-500">Privacy Policy</li>
-            <li className="hover:text-gray-500">Refund and Returns Policy</li>
-            <li className="hover:text-gray-500">Terms & Conditions</li>
-            <li className="hover:text-gray-500">Disclaimer</li>
+            <Link to="/" className="hover:text-gray-500">
+              Antivirus Shop
+            </Link>{' '}
+            <br />
+            <Link to="/privacypolicy" className="hover:text-gray-500">
+              Privacy Policy
+            </Link>{' '}
+            <br />
+            <Link to="/refund&return" className="hover:text-gray-500">
+              Refund and Returns Policy
+            </Link>{' '}
+            <br />
+            <Link to="/t&c" className="hover:text-gray-500">
+              Terms & Conditions
+            </Link>{' '}
+            <br />
+            <Link to="/disclaimer" className="hover:text-gray-500">
+              Disclaimer
+            </Link>
           </ul>
         </div>
         <div>
